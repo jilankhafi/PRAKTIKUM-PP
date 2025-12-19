6ce8afb4a11c3358cf477369626f6ee5d4c603d7
jilankhafi <jilankhafi@gmail.com>
2025-12-20 01:22:42 +0700

    add file matriks.cpp

---
+++ C++/matriks.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matriks[100][100];
+    int jum_baris, jum_kolom, i, j;
+
+    cout << "Input Jumlah Baris Matriks : ";
+    cin >> jum_baris;
+
+    cout << "Input Jumlah Kolom Matriks : ";
+    cin >> jum_kolom;
+
+    for (i = 0; i < jum_baris; i++) {
+        for (j = 0; j < jum_kolom; j++) {
+            cout << "Baris " << i + 1 << ", Kolom " << j + 1 << " = ";
+            cin >> matriks[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << "Hasil Matriks:" << endl;
+
+    for (i = 0; i < jum_baris; i++) {
+        for (j = 0; j < jum_kolom; j++) {
+            cout << matriks[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
